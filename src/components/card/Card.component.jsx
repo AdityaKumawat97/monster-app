1894dfa86875c17800745a9e328d89a08c25cc1c
Aditya_kumawat <31047132+AdityaKumawat97@users.noreply.github.com>
2020-05-13 16:13:22 +0530

    Robohash images added onto the card component

--- src/components/card/Card.component.jsx
+++ src/components/card/Card.component.jsx
@@ -3,8 +3,11 @@ import './Card.styles.css'
 
 export function Card(props) {
     return (
-        <div className='card-container'>
-            <h1>{props.monster.name}</h1>
-        </div>
+        // let { monster } = this.props
+            < div className = 'card-container' >
+                <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="" />
+                <h2>{props.monster.name}</h2>
+                <p>{props.monster.email}</p>
+        </div >                 
     )
 }
