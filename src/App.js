3acc26953916ed8c646aa11bc0914426536de4e4
Aditya_kumawat <31047132+AdityaKumawat97@users.noreply.github.com>
2020-05-13 17:23:44 +0530

    seperating search box component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import { CardList } from './components/card-list/Card-list.component'
+import { SearchBox } from './components/search-box/Search-box.component'
 
 export default class App extends Component {
   constructor(props) {
@@ -22,17 +23,15 @@ export default class App extends Component {
 
 
   render() {
-    let { monsters,searchField } = this.state
-    const filteredMonsters = monsters.filter(monster=>monster.name.toLowerCase().includes(searchField.toLowerCase())
+    let { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
     )
     return (
       <div className="App">
-        <input type='search' 
-        placeholder='search monsters' 
-        onChange={e => { 
-          this.setState({searchField: e.target.value})}
-          } />
-        {/* {console.log(this.state)} */}
+        <SearchBox
+          placeholder='Search Monsters'
+          handleChange={e =>this.setState({ searchField: e.target.value })}
+        />
         <CardList monsters={filteredMonsters} />
       </div>
     )
--- src/components/search-box/Search-box.component.jsx
+++ src/components/search-box/Search-box.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import './Search-box.styles.css'
 
-export function SearchBox() {
+export function SearchBox({ placeholder, handleChange }) {
     return (
         <div>
-            
+            <input className='search'
+                type='search'
+                placeholder={placeholder}
+                onChange={handleChange}
+            />
         </div>
     )
 }
